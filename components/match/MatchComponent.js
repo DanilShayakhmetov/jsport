96ee50b7bf7cd8d8018eaed38c4546858672671c
Danil <abraham_lincoln5@rambler.ru>
2021-03-22 01:13:46 +0300

    Events tab data fetched

--- components/match/MatchComponent.js
+++ components/match/MatchComponent.js
@@ -12,12 +12,13 @@ import {FriendsContext} from '../../FriendsContext';
 import Handler from '../../graphql/handler';
 
 const handler = Handler;
-const d = <Text>{'gggggggggggggggggggg'}</Text>;
+
 export default class MatchScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      focusedTab: '',
+      focusedTab: '0',
+      eventsList: '',
     };
   }
 
@@ -33,10 +34,97 @@ export default class MatchScreen extends Component {
       });
   }
 
-  eventsPage = () => {
+  eventPreparer = (match) => {
+    let eventsList = [];
+    let goalItems = match.goals;
+    let yellowCardItems = match.yellowCards;
+    let redCardItems = match.redCards;
+    let shootoutItems = match.shootouts;
+
+    if (goalItems !== undefined) {
+      goalItems.forEach(function (goal) {
+        let eventItem = {
+          time: '',
+          event: '',
+        };
+        eventItem.event = goal.situation + goal.player_id;
+        eventItem.time = goal.minute;
+        eventsList.push(eventItem);
+      });
+    }
+
+    if (yellowCardItems !== undefined) {
+      yellowCardItems.forEach(function (card) {
+        let eventItem = {
+          time: '',
+          event: '',
+        };
+        eventItem.event = card.reason + card.player_id;
+        eventItem.time = card.minute;
+        eventsList.push(eventItem);
+      });
+    }
+
+    if (redCardItems !== undefined) {
+      redCardItems.forEach(function (card) {
+        let eventItem = {
+          time: '',
+          event: '',
+        };
+        eventItem.event = card.reason + card.player_id;
+        eventItem.time = card.minute;
+        eventsList.push(eventItem);
+      });
+    }
+
+    if (shootoutItems !== undefined) {
+      shootoutItems.forEach(function (card) {
+        let eventItem = {
+          time: '',
+          event: '',
+        };
+        eventItem.event = card.result + card.player_id;
+        eventItem.time = card.minute;
+        eventsList.push(eventItem);
+      });
+    }
+
+    for (let i = 0; i < eventsList.length - 1; i++) {
+      for (let j = 1; j < eventsList.length; j++) {
+        if (eventsList[i].time > eventsList[j].time) {
+          let buff = eventsList[i];
+          eventsList[i] = eventsList[j];
+          eventsList[j] = buff;
+        }
+      }
+    }
+
+    return eventsList;
+  };
+
+  EventsPage = (props) => {
     this.setState({
       focusedTab: 'events',
     });
+    let events = this.eventPreparer(props.match);
+
+    if (events !== undefined) {
+      return (
+        <View>
+          {events.map((item) => (
+            <Text>
+              {item.event}.{item.time}
+            </Text>
+          ))}
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text>events error</Text>
+        </View>
+      );
+    }
   };
 
   statisticsPage = () => {
@@ -51,9 +139,16 @@ export default class MatchScreen extends Component {
     });
   };
 
+  tabsHandler = (page) => {
+    console.log(this.context.focusedTab);
+    this.setState({
+      focusedTab: page,
+    });
+  };
+
   render() {
-    console.log(this.context.matchData._W);
-    var matchD = this.context.matchData._W;
+    console.log(this.state.focusedTab);
+    const matchD = this.context.matchData._W;
     if (
       this.context.matchId === 'empty' ||
       this.context.matchId === undefined ||
@@ -69,47 +164,77 @@ export default class MatchScreen extends Component {
         </View>
       );
     } else {
-      console.log(this.context.matchId);
-      // console.log(this.context.matchData);
-      console.log(matchD);
+      let eventsList = this.eventPreparer(matchD);
       return (
         <View style={styles.container}>
-          {d}
-          {/*<View style={styles.containerTop} key={'qwe'}>*/}
-          {/*  <Text>{this.context.matchId}</Text>*/}
-          {/*  <Text>{matchD.item.start_dt}</Text>*/}
-          {/*  <Text>*/}
-          {/*    {matchD.team1.logo}.{matchD.gf}.{'      :      '}.{matchD.ga}.*/}
-          {/*    {matchD.team2.logo}*/}
-          {/*  </Text>*/}
-          {/*  <Text>*/}
-          {/*    {matchD.team1.short_name}.{'            '}.*/}
-          {/*    {matchD.team2.short_name}*/}
-          {/*  </Text>*/}
-          {/*</View>*/}
-          {/*<View style={{height: 30, marginBottom: 30}}>*/}
-          {/*  <ScrollView horizontal={true} style={styles.scrollItem}>*/}
-          {/*    <TouchableOpacity*/}
-          {/*      activeOpacity={0.8}*/}
-          {/*      onPress={() => this.eventsPage()}>*/}
-          {/*      <Text>{'        События         '}</Text>*/}
-          {/*    </TouchableOpacity>*/}
-          {/*    <TouchableOpacity*/}
-          {/*      activeOpacity={0.8}*/}
-          {/*      style={styles.touchItem}*/}
-          {/*      onPress={() => this.statisticsPage()}>*/}
-          {/*      <Text>{'        Статистика          '}</Text>*/}
-          {/*    </TouchableOpacity>*/}
-          {/*    <TouchableOpacity*/}
-          {/*      activeOpacity={0.8}*/}
-          {/*      onPress={() => this.rosterPage()}>*/}
-          {/*      <Text>{'        Составы          '}</Text>*/}
-          {/*    </TouchableOpacity>*/}
-          {/*  </ScrollView>*/}
-          {/*</View>*/}
-          {/*<View style={styles.mainDataContainer}>*/}
-          {/*  <Text>{this.state.focusedTab}</Text>*/}
-          {/*</View>*/}
+          <View style={styles.containerTop} key={'qwe'}>
+            <Text>{this.context.matchId}</Text>
+            <Text>{matchD.start_dt}</Text>
+            <Text>
+              {matchD.team1.logo}.{matchD.gf}.{'      :      '}.{matchD.ga}.
+              {matchD.team2.logo}
+            </Text>
+            <Text>
+              {matchD.team1.short_name}.{'            '}.
+              {matchD.team2.short_name}
+            </Text>
+          </View>
+          <View style={{height: 30, marginBottom: 30}}>
+            <ScrollView horizontal={true} style={styles.scrollItem}>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={this.tabsHandler.bind(this, '0')}>
+                <Text>{'        События         '}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.touchItem}
+                onPress={this.tabsHandler.bind(this, '1')}>
+                <Text>{'        Статистика          '}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={this.tabsHandler.bind(this, '2')}>
+                <Text>{'        Составы          '}</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
+          <View style={styles.mainDataContainer}>
+            <Text>{this.state.focusedTab}</Text>
+            <View
+              style={{
+                display: this.state.focusedTab === '0' ? null : 'none',
+                overflow: 'hidden',
+              }}>
+              {eventsList.map((item) => (
+                <Text>
+                  {item.event}.{item.time}.{'events'}
+                </Text>
+              ))}
+            </View>
+            <View
+              style={{
+                display: this.state.focusedTab === '1' ? null : 'none',
+                overflow: 'hidden',
+              }}>
+              {eventsList.map((item) => (
+                <Text>
+                  {item.event}.{item.time}.{'stats'}
+                </Text>
+              ))}
+            </View>
+            <View
+              style={{
+                display: this.state.focusedTab === '2' ? null : 'none',
+                overflow: 'hidden',
+              }}>
+              {eventsList.map((item) => (
+                <Text>
+                  {item.event}.{item.time}.{'roast'}
+                </Text>
+              ))}
+            </View>
+          </View>
           {/*<Button*/}
           {/*  title="К списку матчей"*/}
           {/*  onPress={() => this.props.navigation.navigate('MatchCenter')}*/}
